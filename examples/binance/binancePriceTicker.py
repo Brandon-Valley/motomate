a5a62a1b8bd57d064daedeb37433365e4f75ad9f
Brandon Valley <bavvh8@mst.edu>
2018-01-20 14:14:47 -0600

    can click to open an example html page in chrome

--- examples/binance/binancePriceTicker.py
+++ examples/binance/binancePriceTicker.py
@@ -1,4 +1,5 @@
 #https://github.com/sammchardy/python-binance
+# klines stuff:  https://sammchardy.github.io/binance/2018/01/08/historical-data-download-binance.html
 from binance.client import Client
 
 api_key = '4jRVOPdfafcrVP5Ke3DL1g4RKleTTlwirQDrlADivdKjZSaUHsXlZ6XMjfXS95oZ'
